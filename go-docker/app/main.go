3a47915027bf284bf38d226d2d79fcb4f1875753
a-tomohiro <extreme.azisiotaro@gmail.com>
2021-12-02 18:34:26 +0900

    creat db container

--- go-docker/app/main.go
+++ go-docker/app/main.go
@@ -1,25 +1,64 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
+	// _ "github.com/go-sql-driver/mysql"
 )
 
 // Embed htmlファイルに埋め込むデータ構造体
 type Embed struct {
 	Title   string
 	Message string
+	Users   map[int]User
 	Time    time.Time
 }
 
+// User db users
+type User struct {
+	ID       int
+	Name     string
+	Password string
+}
+
+const (
+	// DriverName ドライバ名(mysql固定)
+	DriverName = "mysql"
+	// DataSourceName user:password@tcp(container-name:port)/dbname
+	DataSourceName = "root:golang@tcp(mysql-container:3306)/golang_db"
+)
+
+var usr = make(map[int]User)
 var templates = make(map[string]*template.Template)
 
 func main() {
+	// database
+	db, dbErr := sql.Open(DriverName, DataSourceName)
+	if dbErr != nil {
+		log.Print("error connecting to database:", dbErr)
+	}
+	defer db.Close()
+	rows, queryErr := db.Query("SELECT * FROM users")
+	if queryErr != nil {
+		log.Print("query error :", queryErr)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u User
+		if err := rows.Scan(&u.ID, &u.Name, &u.Password); err != nil {
+			log.Print(err)
+		}
+		usr[u.ID] = User{
+			ID:       u.ID,
+			Name:     u.Name,
+			Password: u.Password,
+		}
+	}
+	// web_server
 	port := "8080"
-
 	templates["index"] = loadTemplate("index")
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
@@ -29,8 +68,7 @@ func main() {
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("timee %+v", time.Now())
-	temp := Embed{"Hello Golang!", "ようこそ、こちら側", time.Now()}
+	temp := Embed{"Hello Golang!", "こんにちは！", usr, time.Now()}
 	if err := templates["index"].Execute(w, temp); err != nil {
 		log.Printf("failed to execute template: %v", err)
 	}
